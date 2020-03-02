4d163ad2d3793f0249868b920ff046a4e35a866e
maiev23 <maiev0691@gmail.com>
2020-03-02 21:47:49 +0900

    로긴 토큰 옵션 수정

--- routes/login.js
+++ routes/login.js
@@ -10,8 +10,10 @@ const secretObj = require("../config/jwt");
 // 1.jwt 객체의 sign()메서드를 호출해서 토큰을 생성한다.
 router.post('/', async (req, res) => {
     let token = jwt.sign({
-        username: "abc@gmail.com"  // 토근의 내용(payload)
-    }, secretObj.secret); //비밀 키 
+        username: req.body.username  // 토근의 내용(payload)
+    }, secretObj.secret, {
+      expiresIn: '5m'
+    }); //비밀 키 
 
     //2. sequelize를 사용해서 요청한 이메일 주소에 해당하는 정보를 DB에서 조회합니다.
     users.findOne({
@@ -21,7 +23,7 @@ router.post('/', async (req, res) => {
     })
         .then(user => {
           if (user.password === req.body.password) {
-            res.cookie("user: ", token);
+            res.cookie("user", token);
             res.json({
                 token: token
             })
