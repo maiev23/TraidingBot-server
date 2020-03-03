ff226d35e62fc7704dbddb7f39ada18552950a21
maiev23 <maiev0691@gmail.com>
2020-03-03 20:21:28 +0900

    로긴 토큰 조건 수정

--- controller/users/login.js
+++ controller/users/login.js
@@ -1,10 +1,13 @@
 const { users } = require('../../models');
-
+const jwt = require('jsonwebtoken')
+const secretObj = require('../../config/jwt');
 module.exports = {
     post: (req, res) => {
         let token = jwt.sign({
-            username: "abc@gmail.com"  // 토근의 내용(payload)
-        }, secretObj.secret); //비밀 키 
+            username: req.body.username  // 토근의 내용(payload)
+        }, secretObj.secret, {
+            expiresIn: '30m'
+          }); //비밀 키 
         //2. sequelize를 사용해서 요청한 이메일 주소에 해당하는 정보를 DB에서 조회합니다.
 
         users.findOne({
@@ -14,13 +17,15 @@ module.exports = {
         })
             .then(user => {
                 if (user.password === req.body.password) {
-                    res.cookie("user: ", token);
                     res.json({
                         token: token
                     })
                 } else {
                     res.status(409).send("email doesn't exist");
                 }
+            })
+            .catch(err =>{
+                res.status(409).send(err);
             })
 
     }
