302d614e84431071f2de6eed5dcdebefe478a770
unknown <jk3877@columbia.edu>
2020-03-03 21:52:44 -0800

    refresh token 1st version

--- controller/users/login.js
+++ controller/users/login.js
@@ -4,11 +4,17 @@ const secretObj = require("../../config/jwt.js");
 
 module.exports = {
     post: (req, res) => {
-        let token = jwt.sign({
+        let AccessToken = jwt.sign({
             username: req.body.username  // 토근의 내용(payload)
         }, secretObj.secret, {
-            expiresIn: '30min'
-          }); //비밀 키 
+            expiresIn: '1h'
+        }); //비밀 키 
+
+        let RefreshToken = jwt.sign({
+            username: req.body.username  // 토근의 내용(payload)
+        }, secretObj.secret, {
+            expiresIn: '168h'
+        });
         //2. sequelize를 사용해서 요청한 이메일 주소에 해당하는 정보를 DB에서 조회합니다.
         users.findOne({
             where: {
@@ -18,7 +24,8 @@ module.exports = {
         })
             .then(user => {
                 if (user.password === req.body.password) {
-                    res.send(token);
+                    res.send(AccessToken);
+                    res.send(RefreshToken);
                 } else {
                     res.status(409).send("email doesn't exist");
                 }
